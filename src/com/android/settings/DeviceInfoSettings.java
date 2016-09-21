aba4446ef28f79619410f721ea5b3b24064f0463
klozz <klozz707@gmail.com>
2016-09-21 11:19:29 +0000

    XPerience version
    Signed-off-by: klozz <klozz707@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -73,6 +73,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
     private static final String KEY_MBN_VERSION = "mbn_version";
     private static final String PROPERTY_MBN_VERSION = "persist.mbn.version";
+    private static final String KEY_MOD_BUILD_DATE = "build_date";
+    private static final String KEY_XPE_VERSION = "xpe_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -125,6 +127,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_MBN_VERSION, PROPERTY_MBN_VERSION);
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_MBN_VERSION,
                 PROPERTY_MBN_VERSION);
+        setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        setValueSummary(KEY_XPE_VERSION, "ro.xpe.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
